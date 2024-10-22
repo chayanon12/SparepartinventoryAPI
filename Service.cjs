9f24ece939af2c1b35e75ae4caad1fa5d11d6425
Chayanon Is-saard <82950373+chayanon12@users.noreply.github.com>
2024-10-22 18:59:28 +0700

    Update Service.cjs

--- Service.cjs
+++ Service.cjs
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 9002;
+const port = 4002;
 const Common = require("./routes/Common.cjs");
 const cors = require('cors');
 app.use(cors({
